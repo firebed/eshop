1fd55ea62b50691f1e7a8b25c0c5d297ef244488
okangr <okan.giritli@gmail.com>
2021-06-05 00:14:36 +0300

    Deleted unnecessary files Updated Ecommerce service provider

--- src/EcommerceServiceProvider.php
+++ src/EcommerceServiceProvider.php
@@ -2,6 +2,22 @@
 
 namespace Firebed;
 
+use App\Models\Cart\Cart;
+use App\Models\Invoice\Company;
+use App\Models\Invoice\Invoice;
+use App\Models\Location\Country;
+use App\Models\Location\CountryPaymentMethod;
+use App\Models\Location\CountryShippingMethod;
+use App\Models\Product\Category;
+use App\Models\Product\CategoryChoice;
+use App\Models\Product\CategoryProperty;
+use App\Models\Product\Manufacturer;
+use App\Models\Product\Product;
+use App\Models\Product\VariantType;
+use App\Models\Settings;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Relations\Relation;
+use Illuminate\Support\Collection;
 use Illuminate\Support\ServiceProvider;
 
 class EcommerceServiceProvider extends ServiceProvider
@@ -13,6 +29,9 @@ class EcommerceServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        $this->app->singleton('countries', fn() => Country::orderBy('name')->get());
+        $this->app->singleton('settings', fn() => Settings::first());
+
         $this->loadViewsFrom(__DIR__ . '/views', 'com');
 
         $this->loadRoutesFrom(__DIR__ . '/routes/web.php');
@@ -25,5 +44,33 @@ class EcommerceServiceProvider extends ServiceProvider
         $this->publishes([__DIR__ . '/resources/lang' => resource_path('lang/vendor/com')], 'lang');
         $this->publishes([__DIR__ . '/resources/views' => resource_path('views/vendor/com')], 'views');
         $this->publishes([__DIR__ . '/public' => public_path('vendor/com')], 'public');
+
+        $this->assignMorphs();
+
+        Collection::macro('toggle', fn($item) => $this->contains($item) ? $this->except($item->id) : $this->concat([$item]));
     }
+
+    private function assignMorphs(): void
+    {
+        Relation::morphMap([
+            'user' => User::class,
+
+            'category'          => Category::class,
+            'category_choice'   => CategoryChoice::class,
+            'category_property' => CategoryProperty::class,
+
+            'product'         => Product::class,
+            'variant_type'    => VariantType::class,
+            //
+            'payment_method'  => CountryPaymentMethod::class,
+            'shipping_method' => CountryShippingMethod::class,
+            //
+            'manufacturer'    => Manufacturer::class,
+            //
+            'cart'            => Cart::class,
+            'invoice'         => Invoice::class,
+            'company'         => Company::class
+        ]);
+    }
+
 }
