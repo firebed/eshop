f3f0257f5206c54a3ea0acc3d3df157e014bb592
okangr <okan.giritli@gmail.com>
2021-06-05 02:46:58 +0300

    Prefix category and product groups

--- src/routes/web.php
+++ src/routes/web.php
@@ -22,12 +22,12 @@ Route::group(['prefix' => '{lang}', 'middleware' => 'locale'], function () {
     Route::view('/account/orders', 'customer.account.orders.index')->name('account.orders.index');
     Route::view('/account/orders/{order}', 'customer.account.orders.show')->name('account.orders.show');
 
-    Route::get('{category:slug}/m/{manufacturers}/{filters}', CategoryController::class)->name('customer.categories.manufacturers.filters');
-    Route::get('{category:slug}/m/{manufacturers}', CategoryController::class)->name('customer.categories.manufacturers');
-    Route::get('{category:slug}/f/{filters}', CategoryController::class)->name('customer.categories.filters');
-    Route::get('{category:slug}', CategoryController::class)->name('customer.categories.show');
+    Route::get('categories/{category:slug}/m/{manufacturers}/{filters}', CategoryController::class)->name('customer.categories.manufacturers.filters');
+    Route::get('categories/{category:slug}/m/{manufacturers}', CategoryController::class)->name('customer.categories.manufacturers');
+    Route::get('categories/{category:slug}/f/{filters}', CategoryController::class)->name('customer.categories.filters');
+    Route::get('categories/{category:slug}', CategoryController::class)->name('customer.categories.show');
 
-    Route::get('{category:slug}/{product:slug}', [ProductController::class, 'show'])->name('customer.products.show');
+    Route::get('products/{category:slug}/{product:slug}', [ProductController::class, 'show'])->name('customer.products.show');
 
     Route::as('checkout.')->group(function () {
         Route::get('cart', CheckoutProductController::class)->name('products.index');
