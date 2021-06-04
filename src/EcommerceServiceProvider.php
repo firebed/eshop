52ef2376de4cf56b409e557e923ac7a83cf1484a
okangr <okan.giritli@gmail.com>
2021-06-05 02:57:09 +0300

    Views setup

--- src/EcommerceServiceProvider.php
+++ src/EcommerceServiceProvider.php
@@ -17,11 +17,15 @@ use Ecommerce\Models\Product\Product;
 use Ecommerce\Models\Product\VariantType;
 use Ecommerce\Models\Settings;
 use Ecommerce\Models\User;
+use Ecommerce\Repository\CartRepository;
+use Ecommerce\Repository\Contracts\CartContract;
+use Ecommerce\Repository\Contracts\Order;
 use Ecommerce\Repository\Contracts\ProductContract;
 use Ecommerce\Repository\ProductRepository;
 use Ecommerce\View\Components\CategoryBreadcrumb;
 use Illuminate\Database\Eloquent\Relations\Relation;
 use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\ServiceProvider;
 use Livewire\Livewire;
 
@@ -35,6 +39,36 @@ class EcommerceServiceProvider extends ServiceProvider
     public function boot(): void
     {
         $this->app->bind(ProductContract::class, ProductRepository::class);
+
+        $this->app->singleton(CartContract::class, CartRepository::class);
+
+        $this->app->singleton(Order::class, function () {
+            if (Auth::check()) {
+                return auth()->user()->activeCart()->firstOrNew();
+            }
+
+//            if (session()->has('cart-session-id')) {
+//                $sessionId = session('cart-session-id');
+//                $cart = Cart::find($sessionId);
+//                if ($cart !== NULL && !$cart->isSubmitted()) {
+//                    return $cart;
+//                }
+//                session()->forget('cart-session-id');
+//            }
+
+            $request = request();
+            if ($request && $request->hasCookie('cart-cookie-id')) {
+                $cookieId = $request->cookie('cart-cookie-id');
+                $cart = Cart::firstWhere('cookie_id', $cookieId);
+                if ($cart !== NULL && !$cart->isSubmitted()) {
+//                    session()->put('cart-session-id', $cart->id);
+                    return $cart;
+                }
+                cookie()->queue(cookie()->forget('cart-cookie-id'));
+            }
+
+            return new Cart();
+        });
 
         $this->app->singleton('countries', fn() => Country::orderBy('name')->get());
         $this->app->singleton('settings', fn() => Settings::first());
