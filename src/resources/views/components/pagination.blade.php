f20b74516603ebd0a68e0a7ef6429e891b28057a
okangr <okan.giritli@gmail.com>
2021-06-07 22:12:09 +0300

    Deleted unnecessary files Updated pagination transactions

--- src/resources/views/components/pagination.blade.php
+++ src/resources/views/components/pagination.blade.php
@@ -4,7 +4,7 @@
 ])
 
 <div class="d-flex justify-content-between align-items-center px-2">
-    <div class="small">{{ __('pagination.showing', ['first' => $paginator->firstItem() ?? 0, 'last' => $paginator->lastItem() ?? 0, 'total' => $paginator->total()]) }}</div>
+    <div class="small">{{ __('eshop::pagination.showing', ['first' => $paginator->firstItem() ?? 0, 'last' => $paginator->lastItem() ?? 0, 'total' => $paginator->total()]) }}</div>
 
     {{ $paginator->onEachSide($onEachSide)->links() }}
 </div>
