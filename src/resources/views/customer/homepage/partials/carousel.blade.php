bbd3d4b88e78d77f1f1a419b3324b562b55dc24e
okangr <okan.giritli@gmail.com>
2021-06-07 21:47:26 +0300

    Removed forgotten content

--- src/resources/views/customer/homepage/partials/carousel.blade.php
+++ src/resources/views/customer/homepage/partials/carousel.blade.php
@@ -1,25 +1,10 @@
 <div id="main-carousel" class="carousel carousel-dark slide" data-bs-ride="carousel">
     <div class="carousel-inner">
-        <div class="carousel-item active">
-            <div class="ratio ratio-21x9 border">
-                <img src="https://handmadenimata.gr/data/homepage/1522923379alize_banner2.jpg" alt="...">
-            </div>
-        </div>
-        <div class="carousel-item">
-            <div class="ratio ratio-21x9 border">
-                <img src="https://alize.gen.tr/banner2_images/1609750107cottoncold_tr.jpg" alt="...">
-            </div>
-        </div>
-        <div class="carousel-item">
-            <div class="ratio ratio-21x9 border">
-                <img src="https://handmadenimata.gr/data/homepage/1522933984banner2.jpg" alt="...">
-            </div>
-        </div>
-        <div class="carousel-item">
-            <div class="ratio ratio-21x9 border">
-                <img src="https://handmadenimata.gr/data/homepage/1522935919cottongoldplusmulticolor.jpg" alt="...">
-            </div>
-        </div>
+{{--        <div class="carousel-item active">--}}
+{{--            <div class="ratio ratio-21x9 border">--}}
+{{--                <img src="https://handmadenimata.gr/data/homepage/1522923379alize_banner2.jpg" alt="...">--}}
+{{--            </div>--}}
+{{--        </div>--}}
     </div>
     <a class="carousel-control-prev" href="#main-carousel" role="button" data-bs-slide="prev">
         <span class="carousel-control-prev-icon" aria-hidden="true"></span>
