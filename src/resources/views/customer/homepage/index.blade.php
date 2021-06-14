9446ff68db6777c392c9a39d77b190d23260f03a
okangr <okan.giritli@gmail.com>
2021-06-14 23:05:58 +0300

    Small fixes

--- src/resources/views/customer/homepage/index.blade.php
+++ src/resources/views/customer/homepage/index.blade.php
@@ -1,4 +1,4 @@
-@extends('eshop::customer.layouts.master', ['title' => __("Home")])
+@extends('eshop::customer.layouts.master', ['title' => __('company.job')])
 
 @push('header_scripts')
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/keen-slider@latest/keen-slider.min.css" />
--- src/resources/views/customer/layouts/master.blade.php
+++ src/resources/views/customer/layouts/master.blade.php
@@ -8,6 +8,8 @@
 
     <title>{{ $title }} - {{ config('app.name') }}</title>
 
+    @includeIf('partials.favicon')
+
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" integrity="sha512-+4zCK9k+qNFUR5X+cKL9EIR+ZOhtIloNl9GIKS57V1MyNsYpYcUrUeQc9vNfzsWfV28IaLL3i96P9sdNyeRssA==" crossorigin="anonymous"/>
 
     <link href="{{ asset('vendor/eshop/css/customer/app.css') }}" rel="stylesheet">
