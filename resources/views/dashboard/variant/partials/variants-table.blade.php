7f930868884d8ddadb09ad55e69cd21c17e6e7c7
okangr <okan.giritli@gmail.com>
2021-10-12 20:25:12 +0300

    Visual updates

--- resources/views/dashboard/variant/partials/variants-table.blade.php
+++ resources/views/dashboard/variant/partials/variants-table.blade.php
@@ -40,15 +40,17 @@
                     <em class="fas fa-eye {{ $variant->visible ? 'text-teal-500' : 'text-gray-400' }}"></em>
                     <div class="d-grid gap-1">
                         <em class="fas fa-shopping-cart {{ $variant->available ? 'text-teal-500' : 'text-gray-400' }}"></em>
-                        @if($variant->available_gt !== "" && $variant->available_gt !== null) 
-                            <small class="fw-500 text-center lh-sm bg-secondary rounded-pill text-light px-1" style="font-size: .7rem">{{ $variant->available_gt }}</small> 
+                        @if($variant->available_gt !== "" && $variant->available_gt !== null)
+                            <span class="border-top"></span>
+                            <small class="fw-500 text-center lh-sm" style="font-size: .7rem">{{ $variant->available_gt }}</small> 
                         @endif
                     </div>
 
                     <div class="d-grid gap-1">
                         <em class="fas fa-list-ol {{ $variant->display_stock ? 'text-teal-500' : 'text-gray-400' }}"></em>
                         @if($variant->display_stock_lt !== "" && $variant->display_stock_lt !== null) 
-                            <small class="fw-500 text-center lh-sm bg-secondary rounded-pill text-light px-1" style="font-size: .7rem">{{ $variant->display_stock_lt }}</small> 
+                            <span class="border-top"></span>
+                            <small class="fw-500 text-center lh-sm" style="font-size: .7rem">{{ $variant->display_stock_lt }}</small> 
                         @endif
                     </div>
                 </div>
