8f3cf545f3da48587b7fe55f8275f54bf709109f
okangr <okan.giritli@gmail.com>
2021-11-19 03:11:17 +0200

    Style fixes

--- resources/views/customer/product/partials/product-category.blade.php
+++ resources/views/customer/product/partials/product-category.blade.php
@@ -1,4 +1,4 @@
-<div class="small hstack gap-2">
+<div class="d-flex hstack gap-2">
     <span class="text-secondary">{{ __("Category") }}: </span>
     <a href="{{ categoryRoute($category) }}" class="text-decoration-none">{{ $category->name }}</a>
 </div>
--- resources/views/customer/product/partials/product-parent.blade.php
+++ resources/views/customer/product/partials/product-parent.blade.php
@@ -1,4 +1,4 @@
-<div class="small hstack gap-2">
+<div class="small d-flex gap-2">
     <span class="text-secondary">{{ __("Collection") }}:</span>
     <a href="{{ route('products.show', [app()->getLocale(), $category->slug, $product->parent->slug]) }}" class="text-decoration-none">{{ $product->parent->name }}</a>
 </div>
