984e31f1e4604343eb532a94824abe62826b6c0d
okangr <okan.giritli@gmail.com>
2021-11-20 05:00:10 +0200

    Bug fixes

--- resources/views/customer/emails/order/partials/logo.blade.php
+++ resources/views/customer/emails/order/partials/logo.blade.php
@@ -1,3 +1,3 @@
 <div style="margin-bottom: 1rem; text-align: center">
-    <img alt="{{ __("app.name") }}" src="{{ asset(eshop('logo.path')) }}" style="max-width: {{ eshop('logo.width') }}px; max-height: {{ eshop('logo.height') }}px">
+    <img alt="{{ __("app.name") }}" src="{{ asset('images/logo.webp') }}" style="max-width: 306px; max-height: 76px">
 </div>
