06dcea655d562e46572be7e2e95944eb54b861b1
okangr <okan.giritli@gmail.com>
2021-11-27 12:56:45 +0200

    Styling customer notes in cart view

--- resources/views/dashboard/cart/wire/customer-notes.blade.php
+++ resources/views/dashboard/cart/wire/customer-notes.blade.php
@@ -1,5 +1,5 @@
 <div>
-    <x-bs::navbar expand="xxl" class="card shadow-sm flex-xxl-wrap">
+    <x-bs::navbar expand="xxl" class="card shadow-sm flex-xxl-wrap {{ filled($notes) ? 'bg-yellow-100' : '' }}">
         <x-bs::navbar.brand class="d-xxl-flex justify-content-xxl-between w-xxl-100">{{ __('Notes') }}</x-bs::navbar.brand>
 
         <x-bs::navbar.toggler target="customer-notes"/>
@@ -8,11 +8,17 @@
             <div class="d-grid">
                 <a href="#" class="text-decoration-none" wire:click="$toggle('showModal')">{{ __("Edit") }}</a>
 
-                <div class="text-secondary">{{ !empty($notes) ? $notes : __("No notes from customer") }}</div>
+                <div class="mt-3">
+                    @if(filled($notes))
+                        <strong>{{ $notes }}</strong>
+                    @else
+                        <span class="text-secondary">{{ __("No notes from customer") }}</span>
+                    @endif
+                </div>
             </div>
         </x-bs::navbar.collapse>
     </x-bs::navbar>
-    
+
     <form wire:submit.prevent="save">
         <x-bs::modal wire:model.defer="showModal">
             <x-bs::modal.header>{{ __('Customer notes') }}</x-bs::modal.header>
